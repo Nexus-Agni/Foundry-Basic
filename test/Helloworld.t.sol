f7b9c2cec7647cb5a1f3303c61d840bc6ecd982e
Nexus-Agni <agnibhachakraborty11@gmail.com>
2025-03-23 10:30:03 +0530

    feat: add HelloWorld contract and corresponding tests

---
+++ test/Helloworld.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+
+import {Test, console} from "forge-std/Test.sol";
+import {HelloWorld} from "../src/HelloWorld.sol";
+
+
+contract HelloWorldTest is Test {
+    HelloWorld public helloWorld;
+
+
+    function setUp() public {
+        console.log("Setup");
+        helloWorld = new HelloWorld();
+    }
+
+
+    function test_str() public view {
+        console.log("Test STR called");
+        assertEq(helloWorld.str(),"Hello World");
+    }
+
+
+}
